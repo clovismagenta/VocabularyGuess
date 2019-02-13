c14d0f1ef9cf07e87a29ec0327e2b04958ccb18d
Clóvis Magenta da Cunha <clovismagenta@gmail.com>
2019-02-12 22:35:23 -0700

    Initial Commit

---
+++ VocabularyGuess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocabularyGuess
+//
+//  Created by Clovis Magenta da Cunha on 12/02/19.
+//  Copyright © 2019 CMC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
